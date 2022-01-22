4ae4c802e0c1ab311d0438287810135ddf5ad140
kanan4gh <sweet.rosecolor@gmail.com>
2022-01-22 12:06:40 +0900

    線形回帰

--- step41.py
+++ step41.py
@@ -2,6 +2,8 @@ from dezero import Variable
 import dezero.functions as F
 import numpy as np
 
+# 線形回帰対応
+
 x = Variable(np.random.randn(2, 3))
 W = Variable(np.random.randn(3, 4))
 y = F.matmul(x, W)
